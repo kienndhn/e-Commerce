946c64f0bb6ead47085016926c5346f0512fb20b
nguyendinhkien <nguyendinhkien12344321@gmail.com>
2021-09-27 23:48:38 +0700

    modified:   ../backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -67,7 +67,7 @@ if (process.env.NODE_ENV === 'production') {
 app.use(notFound)
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5000
 
 app.listen(
   PORT,
